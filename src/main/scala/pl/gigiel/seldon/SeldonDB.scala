d3d870cb167404daa149fc534b0775061f9ad4e0
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2016-02-12 21:54:20 +0100

    schema, insert

--- src/main/scala/pl/gigiel/seldon/SeldonDB.scala
+++ src/main/scala/pl/gigiel/seldon/SeldonDB.scala
@@ -1,16 +1,25 @@
 package pl.gigiel.seldon
 
 import slick.driver.H2Driver.api._
+import org.h2.tools.Server
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object SeldonDB {
   def db: Unit = {
-    val database = Database.forConfig("seldondb")
+    //Class.forName("org.h2.Driver") // unnecessary in jdbc 4+
+    //var server: Server = Server.createTcpServer("-tcpPort", "9123", "-tcpAllowOthers").start
 
-    val infos: TableQuery[Infos] = TableQuery[Infos]
-    val insertAction: DBIO[Option[Int]] = infos ++= Seq(
-      new Info(Option(1), "info1"), //
-      new Info(Option(2), "info2") //
-      )
+    val infos = TableQuery[Infos]
+    val database = Database.forConfig("seldondb")
+    try {
+      Await.result(database.run(DBIO.seq(
+        //infos.schema.create,
+        infos += Info(None, "info1"),
+        infos += Info(None, "info1"),
+        infos.result.map(println))), Duration.Inf)
+    } finally database.close
 
     val source = scala.io.Source.fromURL(getClass.getResource("/application.conf"))
     println(source.mkString)
--- src/main/scala/pl/gigiel/seldon/Tables.scala
+++ src/main/scala/pl/gigiel/seldon/Tables.scala
@@ -1,9 +1,8 @@
 package pl.gigiel.seldon
 
 import slick.driver.H2Driver.api._
-import slick.lifted.ProvenShape
 
-case class Info(id: Option[Int], info: String)
+case class Info(id: Option[Int] = None, info: String)
 
 class Infos(tag: Tag) extends Table[(Info)](tag, "INFOS") {
   def id: Rep[Int] = column[Int]("ID", O.PrimaryKey, O.AutoInc)
@@ -11,5 +10,3 @@ class Infos(tag: Tag) extends Table[(Info)](tag, "INFOS") {
 
   def * = (id.?, info) <> (Info.tupled, Info.unapply)
 }
-
-//val infos = TableQuery[Infos]
